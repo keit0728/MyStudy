7242d5b3d323d9904402cfd1228809c8beb84dc0
KeishiroNagaseforWin10 <keit0728.job@gmail.com>
2020-11-30 02:56:11 +0900

    APIkey等を削除

--- Nodejs/TrelloAPI/index.js
+++ Nodejs/TrelloAPI/index.js
@@ -4,9 +4,11 @@ const TrelloAPI = require('./Trello/TrelloAPI.js');
 
 // Trello API を使用するために必要
 // https://qiita.com/RYOSUKE310/items/016549922a17376efdf8
-const key = '9903946b5f7184a163ec49d0a49a9196';
-const token = 'ba5cc7c4c6ea52f05b517527f8f3d71d40c5150d9e8340799a6371e80c9dbecf';
-const username = 'keit0728';
+// ↑を参考に下記を3変数を適宜自分の環境に合わせること
+const key = '';
+const token = '';
+const username = '';
+
 TrelloAPI.Init(key, token, username);
 
 // ボードリストを取得してコンソールに出力
