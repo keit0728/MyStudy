63c151e560c8318b58b57e97b827200b5c0959e8
KeishiroNagaseforWin10 <keit0728.job@gmail.com>
2020-11-30 02:51:44 +0900

    TrelloAPIを関数化

--- Nodejs/TrelloAPI/index.js
+++ Nodejs/TrelloAPI/index.js
@@ -1,30 +1,15 @@
 'use strict';   // codeの厳格化
 
-let https = require('https');
-let URL = '';
+const TrelloAPI = require('./Trello/TrelloAPI.js');
 
 // Trello API を使用するために必要
 // https://qiita.com/RYOSUKE310/items/016549922a17376efdf8
 const key = '9903946b5f7184a163ec49d0a49a9196';
 const token = 'ba5cc7c4c6ea52f05b517527f8f3d71d40c5150d9e8340799a6371e80c9dbecf';
 const username = 'keit0728';
+TrelloAPI.Init(key, token, username);
 
-// 上記key等からボード情報を取得するためのURLを発行
-URL = `https://trello.com/1/members/${username}/boards?key=${key}&token=${token}&fields=name`;
-
-//指定のURLからデータ取得
-const req = https.request(URL, (res) => {
-    //WebAPIからjson取得してコンソールに表示
-    res.on('data', (chunk) => {
-        let board = JSON.parse(chunk);
-        console.log(JSON.stringify(board));
-    });
-
-    //取得完了をコンソールに表示
-    res.on('end', () => {
-        console.log('JSONデータは以上です。');
-    });
-
+// ボードリストを取得してコンソールに出力
+TrelloAPI.GetBoardList((boardList) => {
+    console.log(JSON.stringify(boardList));
 });
-
-req.end();
