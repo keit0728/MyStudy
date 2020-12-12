5647d6122044f42066db592d1d1cfe3157424754
KeishiroNagaseforWin10 <keit0728.job@gmail.com>
2020-12-12 14:43:59 +0900

    最後の方のdataを書き込まず捨てていた不具合の修正

--- Nodejs/SplitFile/index.js
+++ Nodejs/SplitFile/index.js
@@ -1,17 +1,16 @@
 'use strict';   // 処理を厳格にする
 
 // モジュール読み込み
-const fs = require('graceful-fs');      // ERROR:Too many open filesを回避できるfs
-                                        // 参考(http://dotnsf.blog.jp/archives/1064268194.html)
+const fs = require('graceful-fs');      // ERROR:Too many open filesを回避できるfs / 参考(http://dotnsf.blog.jp/archives/1064268194.html)
 const readline = require('readline');
 
-const originFilePath = './_tmp/じょうしょうたん6.txt';
-// const originFilePath = './_tmp/test.txt';
+// const originFilePath = './_tmp/じょうしょうたん6.txt';
+const originFilePath = './_tmp/test.txt';
 // const createFilePath = './_tmp/Bunkatsu/dest.txt';
 const createFilePath = './_tmp/Bunkatsu/dest';
 const extension = '.txt';
 
-const lineLimit = 100000;
+const lineLimit = 10;
 let n = 0;
 let tmpN = 0;
 let dataBuf = '';
@@ -19,16 +18,24 @@ let dataBuf = '';
 const rs = fs.createReadStream(originFilePath, 'utf8');
 const rl = readline.createInterface(rs, {});
 
+// 1行ずつ読み込み＆lineLimitだけ読み込んだら書き込み
 rl.on('line', chunk => {
-
     dataBuf = `${dataBuf}${chunk}\n`;
     n++;
-
     if (n == lineLimit) {
         tmpN = tmpN + n;
-        let dest = fs.createWriteStream(`${createFilePath}-${tmpN/n}${extension}`, 'utf8');
+        let dest = fs.createWriteStream(`${createFilePath}-${tmpN/lineLimit}${extension}`, 'utf8');
         dest.write(dataBuf);
         n = 0;
         dataBuf = '';
     }
+});
+
+// 最終行に到達したら最後に溜まっているdataを吐き出して終了
+rl.on('close', () => {
+    let dest = fs.createWriteStream(`${createFilePath}-${(tmpN/lineLimit)+1}${extension}`, 'utf8');
+    dest.write(dataBuf);
+    n = 0;
+    dataBuf = '';
+    // console.log('end');
 });
