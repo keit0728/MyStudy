da59654e9762cbe8838d8e619977fbed39add05a
KeishiroNagaseforWin10 <keit0728.job@gmail.com>
2020-12-13 23:00:21 +0900

    ファイルリスト取得関数を追加

--- Nodejs/BunkatsuKun/file-operation/file-operation.js
+++ Nodejs/BunkatsuKun/file-operation/file-operation.js
@@ -9,46 +9,11 @@
 const fs = require('fs');
 module.exports = {
     /**
-      * @description to get the server configuration
-      * @param  {} callback function name
-      */
-    getFilePath: async (dirpath) => {
-
-        fs.readdir(dirpath, { withFileTypes: true }, (err, dirents) => {
-            return dirents;
-
-            // console.log('hello');
-        });
-        // console.log('hello');
-
-
-
-        // fs.readdir(dirpath, {withFileTypes: true}, (err, dirents) => {
-        //     if (err) {
-        //         console.error(err);
-        //         return;
-        //     }
-
-        //     // return dirents;
-    
-        //     for (const dirent of dirents) {
-
-                
-        //         const fp2 = path.join(dirpath, dirent.name, dirent.name);
-        //         // fs.mkdir(`./_tmp/${dirent.name}`, function (err) {
-        //         //     console.log(err);
-        //         //     // return;
-        //         // });
-    
-        //         const fp = path.join(dirpath, dirent.name);
-        //         if (dirent.isDirectory()) {
-        //             showFiles(fp, callback);
-        //         } else {
-        //             callback(fp);
-        //         }
-        //     }
-        // });
+     * @description ファイルパスを取得
+     * @param  {} callback function name
+     */
+    getFilePath: (dirpath) => {
+        const files= fs.readdirSync(dirpath);
+        return files;
     }
-    
-
 }
