65204c3699a9363b7dac22eb918a24f96f3b0e9f
KeishiroNagaseforWin10 <keit0728.job@gmail.com>
2020-12-14 01:09:55 +0900

    同名ファイルエラー文追加

--- Nodejs/BunkatsuKun/message/message.js
+++ Nodejs/BunkatsuKun/message/message.js
@@ -9,5 +9,6 @@
 
 'use strict';   // 処理を厳格にする
 module.exports = {
-    NO_SPLITTABLE_FILES_FOUND: '分割可能なファイルが存在しません。'
+    NO_SPLITTABLE_FILES_FOUND: '分割可能なファイルが存在しません。',
+    EXIST_SAME_FILENAME: '同名のファイルが存在します。'
 }
