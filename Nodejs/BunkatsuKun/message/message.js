5385167458a9eb74846992d721e27b90d96b3057
KeishiroNagaseforWin10 <keit0728.job@gmail.com>
2020-12-13 23:44:42 +0900

    メッセージモジュール作成

---
+++ Nodejs/BunkatsuKun/message/message.js
@@ -0,0 +1,10 @@
+/**
+ * Project Name : BunkatsuKun
+ * @author      Keit-oDama
+ * @date        Dec 13, 2020
+ * @description メッセージライブラリ
+ */
+'use strict';   // 処理を厳格にする
+module.exports = {
+    NO_SPLITTABLE_FILES_FOUND: '分割可能なファイルが存在しません。'
+}
