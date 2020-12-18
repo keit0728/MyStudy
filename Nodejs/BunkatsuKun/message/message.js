8f14fe7e2f2edd85a92045049b6b96ff8244e8c4
KeishiroNagaseforWin10 <keit0728.job@gmail.com>
2020-12-19 00:21:10 +0900

    log用メッセージの追加

--- Nodejs/BunkatsuKun/message/message.js
+++ Nodejs/BunkatsuKun/message/message.js
@@ -12,4 +12,7 @@ module.exports = {
     NO_SPLITTABLE_FILES_FOUND:  '分割可能なファイルが存在しません。',
     NOT_FOUND_SETTINGFILES:     'settings.iniファイルが存在しません。',
     OUT_OF_TARGET_FILES:        '分割できるファイルの拡張子は.csv、.txtです。',
+    SUCCESS_READ_FILE:          'ファイルの読み込みに成功しました。',
+    START_SPILIT_FILE:          'ファイルの分割を開始しました。',
+    FINISH_SPILIT_FILE:         'ファイルの分割が完了しました。',
 }
