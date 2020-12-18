0d2a2b002f71600f4f4cc91c9d3de205e236c350
KeishiroNagaseforWin10 <keit0728.job@gmail.com>
2020-12-19 00:23:39 +0900

    logを出力できるよう修正

--- Nodejs/BunkatsuKun/index.js
+++ Nodejs/BunkatsuKun/index.js
@@ -14,17 +14,18 @@ const fileOperation = require('./file-operation/file-operation.js');
 const message = require('./message/message.js');
 const propertiesReader = require('properties-reader');
 const fs = require('fs');
-
 const path = require('path');
+require('./logger/logger.js');
 
 
 // コマンドライン引数から分割したいファイルのパスを取得
 let filePath = process.argv[2];
-filePath = "D:\\MyStudy\\Nodejs\\BunkatsuKun\\_test\\test.txt";
+// filePath = "D:\\MyStudy\\Nodejs\\BunkatsuKun\\_test\\test.txt";
 
 
 // ファイルが存在しなければ終了
 if (typeof filePath === "undefined") {
+    logger.error(message.NO_SPLITTABLE_FILES_FOUND);
     throw new Error(message.NO_SPLITTABLE_FILES_FOUND);
 }
 
@@ -33,18 +34,26 @@ if (typeof filePath === "undefined") {
 const extention = path.extname(filePath);
 const regex = /.txt|.csv/gi;
 if (!regex.test(extention)) {
+    logger.error(message.OUT_OF_TARGET_FILES);
     throw new Error(message.OUT_OF_TARGET_FILES);
 }
+logger.info(`${filePath} ${message.SUCCESS_READ_FILE}`);
 
 
 // 設定ファイル読み込み
-if (!fs.existsSync(`${__dirname}\\settings.ini`)) {
+const settingFilePath = `${__dirname}\\settings.ini`;
+if (!fs.existsSync(settingFilePath)) { //ファイルが存在しないなら終了
+    logger.error(message.NOT_FOUND_SETTINGFILES);
     throw new Error(message.NOT_FOUND_SETTINGFILES);
 }
-const properties = propertiesReader(`${__dirname}\\settings.ini`);
+const properties = propertiesReader(settingFilePath);
 const lineLimit = properties.get('settings.lineLimit');
+logger.info(`${settingFilePath} ${message.SUCCESS_READ_FILE}`);
 
 
 // ファイル分割
-console.log(filePath);
+logger.info(message.START_SPILIT_FILE);
+console.log(`${filePath} を分割中...`);
 fileOperation.splitFile(filePath, lineLimit);
+console.log(`完了！`);
+logger.info(message.FINISH_SPILIT_FILE);
